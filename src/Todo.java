2cbc3b46f0f8111bc1c1efd619d00ae91eb1ecf8
Lyndee Hoge <lyndee.hoge@ou.edu>
2019-02-07 09:57:19 -0600

    Initial commit with given java files

---
+++ src/Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-05
+OU ID_______________: 113442594
+OU EMAIL____________: lynden.hoge@ou.edu
+
+Tasks:
+1. XXX
+2. XXX
+
+Estimated Time (minutes):
+1. XXX
+2. XXx
+
+Actual Time (minutes):
+1. XXX
+2. XXX
+*/}
