0f36d4e7ce4bf986eccc789165461960d2ca54e9
Lyndee Hoge <lyndee.hoge@ou.edu>
2019-02-09 16:47:33 -0600

    Updated Todo.java

--- src/Todo.java
+++ src/Todo.java
@@ -1,18 +1,45 @@
 public class Todo {/*
 Lab Section_________: 11
-Last Modified_______: 2019-02-05
+Last Modified_______: 2019-02-09
 OU ID_______________: 113442594
 OU EMAIL____________: lynden.hoge@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Finish Equipment and Animal Class equals methods
+2. Write full constructor test for AnimalTest.java
+3. Write test for getters for AnimalTest.java
+4. Write test for animal.ToString() for AnimalTest.java
+5. Write test for equipment constructor for EquipmentTest.java
+6. Write test for Equipment.toString() for EquipmentTest.java
+7. Write test for Equipment getters for EquipmentTest.java
+8. Write test for Equipment equals for EquipmentTest.java ?
+9. Write test for Zoo getters for ZooTest.java
+10. Write test for Zoo.toString() for ZooTest.java
+11. Write test for animal.equals() for AnimalTest.java ?
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 10
+2. 5
+3. 10
+4. 10
+5. 20
+6. 10
+7. 5
+8. 20
+9. 5
+10. 10
+11. 20
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7. 
+8. 
+9. 
+10. 
+11. 
 */}
